07656eff4b0bb605924cafdbea76e4dcc1b13553
DrSkyFox <52832487+DrSkyFox@users.noreply.github.com>
2022-01-13 19:41:50 +0300

    Initial commit

---
+++ src/main/java/com/example/gamek/GameKApplication.java
@@ -0,0 +1,13 @@
+package com.example.gamek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameKApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gamek/GameKApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gamek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameKApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
